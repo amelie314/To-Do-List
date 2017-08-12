7e872d5ad7f20be92aafa9ae67ec4db0ed363ca0
HuihuaChen <qangela314@gmail.com>
2017-08-13 00:33:40 +0800

    debug

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
 var submitBtn = document.getElementById('submit-btn');
-		var result = document.getElementById('result');
-		submitBtn.addEventListener('click',function(e){
+		var reuslt = document.getElementById('reuslt');
+		submitBtn.addEventListener('click', function(e) {
 			var todoInput = document.getElementById('todo-input').value;
 			console.log(todoInput);
-			if (todoInput !=="") {
+			if(todoInput !== '') {
 				var todo = document.createElement('LI');
-				todo.innerHTML = todoInput + '<button>X</botton>';
-				result.appendChild(todo);
-				document.getElementById('todo-input').value= '' ;
-
-			}else{
+				todo.innerHTML =  todoInput + ' <button>X</button>';
+				reuslt.appendChild(todo);
+				document.getElementById('todo-input').value = '';
+			} else {
 				alert('請輸入值');
 			}
 		});
-		result.addEventListener('click',function(e){
-			console.log(e.target.parentNode.parentNode)
-			e.target.parentNode.parentNode.remove(e.target.parentNode);
+		reuslt.addEventListener('click', function(e) {
+			console.log(e.target.parentNode.parentNode);
+			e.target.parentNode.parentNode.removeChild(e.target.parentNode);
 		});
